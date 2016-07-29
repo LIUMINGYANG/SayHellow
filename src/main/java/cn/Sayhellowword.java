f0444a0545bd71cadaa53aba763a54fef13642b4
LIUMINGYANG <lmyjava@163.com>
2016-07-29 16:05:41 +0800

    上传测试

---
+++ src/main/java/cn/Sayhellowword.java
@@ -0,0 +1,15 @@
+package main.java.cn;
+
+
+public class Sayhellowword {
+
+	public static void SayHellowWord() {
+		System.out.print("SayHellowWord!");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SayHellowWord();
+	}
+
+}
